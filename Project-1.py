ecfe7dce97a4e0dfb6f2552556a3519f5ce24862
Themile95 <jhonwesen8@gmail.com>
2024-12-16 08:50:50 -0800

    Project-1.py

--- Project-1.py
+++ Project-1.py
@@ -1,17 +1,24 @@
-#calculator
+print("Welcome to the rollercoaster!!!")
 
-num_1 = int(input("Enter the first number: "))
-num_2 = int(input("Enter the second number: "))
-op = input("Enter the operator: ")
+height = int(input("Enter your height in cm: "))
+bill = 0
 
-#if_statement
-
-if op == "+":
-    print(str("The answer is") (num_1 + num_2))
-if op == "-":
-    print(str("The answer is") (num_1 - num_2))
-if op == "/":
-    print(str("The answer is") (num_1 / num_2))
-if op == "*":
-    print(str("The answer is") (num_1 * num_2))
+if height >= 120:
+    print("You can ride the roller coaster.")
+    age = int(input("What Is Your Age? "))
+    if age < 12:
+        bill = 5
+        print("Child Tickets Are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth Tickets Are $7.")
+    else:
+        bill = 12
+        print("Adult Tickets Are $12.")
+    photo = input("Do You Want Your Photo? Y or N. ")
+    if photo == "y":
+        bill += 3
+    print(f"Your Final Bill Is ${bill}.")
+else:
+    print("You Can't Ride The Rollercoaster.")
 
